bf95a976bf0a38d56a09b3b92ff0e2ee9746dac0
Cosmin <visantudorcosmin45@yahoo.com>
2017-03-27 18:34:36 +0300

    Zigazag working

--- zigzag.m
+++ zigzag.m
@@ -1,37 +1,106 @@
 function Z = zigzag(n)
 
-Z = zeros(n,n);
+Z = zeros(n);
 crt=0;
 j=1;
 k=1;
+edge=0;
+mid=0
 
-while crt < n*n
-disp("iter")
+while (1)
+   if(Z(1,n) !=0 && Z(n,1) !=0)
+    break
+   endif
+
   if  (j==1 && k ==1) 
     Z(j,k)=crt;
-    crt+=1;
-    k+=1;
-    disp("if1")
-  elseif (j==1 && k!=1)
-    Z(j,k)==crt;
-    disp("if2")
-    while k!=1
-    disp("while1")
-      crt +=1;
-      k-=1;
-      j+=1;
+    k=k+1;
+    
+  elseif (j==1 && k>1)
+    if (edge == 1 && k != n) 
+      crt+=1
       Z(j,k)=crt
+      k=k+1;
+      if (Z(1,n)!=0 && Z(n,1) !=0)
+        break
+      endif
+      endif
+    while (k>1 && j<=n)
+      if (Z(1,n)!=0 && Z(n,1) !=0)
+        break
+      endif
+      crt +=1
+      Z(j,k)=crt
+      k=k-1;
+      j=j+1;
     endwhile
-  elseif (j!=1 && k==1)
-    Z(j,k)==crt;
-    disp("if3")
-    while j!=1
-    disp("while2")
-     crt+=1;
-     k+=1;
-     j-=1;
-     Z(j,k)==crt
+    edge=1;
+  elseif (j>1 && k==1)
+   if (edge == 1 && j!=n)
+      
+      crt+=1
+      Z(j,k)=crt
+      j=j+1;
+      if (Z(1,n)!=0 && Z(n,1) !=0)
+        break
+      endif
+    endif
+    while (j>1 && k<=n)
+      if (Z(1,n)!=0 && Z(n,1) !=0)
+        break
+      endif
+    crt+=1
+     Z(j,k)=crt
+     k=k+1;
+     j=j-1;
     endwhile
+   edge=1;
   endif
 endwhile
+Z=flipud(Z)
+Z=fliplr(Z)
+crt=n*n-1
+edge=0
+j=1
+k=1
+while (1)
+  if (Z(1,n-1)!=0 && Z(n-1,1)!=0)
+    break
+   endif
+
+  if  (j==1 && k ==1) 
+    Z(j,k)=crt;
+    k=k+1;
+    
+  elseif (j==1 && k>1)
+    if (edge == 1 && k != n) 
+      crt-=1
+      Z(j,k)=crt
+      k=k+1;
+
+    endif
+    while (k>1 && j<=n)
+      crt -=1
+      Z(j,k)=crt
+      k=k-1;
+      j=j+1;
+    endwhile
+    edge=1;
+  elseif (j>1 && k==1)
+   if (edge == 1 && j!=n)
+      crt-=1
+      Z(j,k)=crt
+      j=j+1;
+    endif
+    while (j>1 && k<=n)
+    crt-=1
+     Z(j,k)=crt
+     k=k+1;
+     j=j-1;
+    endwhile
+   edge=1;
+  endif
+endwhile
+Z=fliplr(Z)
+Z=flipud(Z)
 endfunction
